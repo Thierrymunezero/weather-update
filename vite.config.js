d01bcab3b7f5e50bb3e8d21c7cf850eeb1261281
Thierrymunezero <167681830+Thierrymunezero@users.noreply.github.com>
2024-11-20 00:02:30 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [react()],
   server: {
-    host: '10.200.17.104',  // Allow connections from the local network
-    port: 3050,      // Specify the port (optional, default is 5173)
+    host: true, 
+    port: process.env.PORT || 3000, 
   },
-})
+  build: {
+    outDir: 'build', 
+  },
+}));
