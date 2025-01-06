1ae2fe3364ce715ca4cf7d3c640c7463753e933b
Thierry Iradukunda <167681830+Thierrymunezero@users.noreply.github.com>
2025-01-06 18:41:28 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => ({
+  base: '/', // Set this to your subpath if needed (e.g., '/subpath/')
   plugins: [react()],
   server: {
-    host: true, 
-    port: process.env.PORT || 4000, 
+    host: true,
+    port: process.env.PORT || 3000,
+  },
+  build: {
+    outDir: 'dist', // Default is 'dist', but you can change it
   },
 }));
